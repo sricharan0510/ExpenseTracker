302aa31282ebf2d3660defadac644945d8f86cd1
Sricharan Illandula <sricharan0504@gmail.com>
2024-12-18 23:04:21 +0530

    AudExp-Commit

--- Exp-Back/Controllers/AudExp.js
+++ Exp-Back/Controllers/AudExp.js
@@ -4,45 +4,33 @@ const users = require("../Models/UserSchema");
 const addExpense = async (req, res) => {
   const { edate, ename, eamt, ecat, epri, epm } = req.body;
   try {
-    const user = await users.findOne({}); 
+    const user = await users.findOne({});
     if (!user) {
       return res.status(404).send({ message: "User not found." });
     }
-    const formattedDate = new Date(edate).toISOString().split('T')[0];
-    if (user.expenses && user.expenses[formattedDate]) {
-      await users.updateOne(
-        { _id: user._id },
-        {
-          $push: {
-            [`expenses.${formattedDate}`]: {
-              expenseName: ename,
-              expenseAmount: eamt,
-              category: ecat,
-              priority: epri,
-              paymentMethod: epm
-            }
-          }
-        }
-      );
-    } else {
-      await users.updateOne(
-        { _id: user._id },
-        {
-          $set: {
-            [`expenses.${formattedDate}`]: [
-              {
-                expenseName: ename,
-                expenseAmount: eamt,
-                category: ecat,
-                priority: epri,
-                paymentMethod: epm
-              }
-            ]
-          }
-        }
-      );
-    }
-    res.status(201).send({ message: "Expense added successfully.",user });
+    const formattedDate = new Date(edate).toISOString();
+    user.allExpenses.map((expObj) => {
+      if (expObj.ExpDate === formattedDate) {
+        expObj.Expenses.push({
+          expenseName: ename,
+          expenseAmount: eamt,
+          category: ecat,
+          priority: epri,
+          paymentMethod: epm
+        });
+      }
+    })
+    user.allExpenses.push({
+      ExpDate: formattedDate,
+      Expenses: [{
+        expenseName: ename,
+        expenseAmount: eamt,
+        category: ecat,
+        priority: epri,
+        paymentMethod: epm
+      }],
+    });
+    res.status(201).send({ message: "Expense added successfully.", user });
   } catch (error) {
     console.error("Error adding expense:", error);
     res.status(500).send({ message: "Error adding expense.", error });
