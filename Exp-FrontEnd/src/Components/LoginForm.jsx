be9d1d6f57bb7c0727555cf766c882dddf3c8a0e
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-05 11:18:56 +0530

    Commit

--- Exp-FrontEnd/src/Components/LoginForm.jsx
+++ Exp-FrontEnd/src/Components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import '../App.css'
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LoginForm() {
     const [userId, setUserId] = useState('');
