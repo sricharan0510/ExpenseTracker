338587b6f78a445c1a49aebfc97cc03f2c148aab
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-10 14:29:48 +0530

    Commit

--- Exp-FrontEnd/src/Components/UserPage.jsx
+++ Exp-FrontEnd/src/Components/UserPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Route, Routes, Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, Link, useParams, useLocation } from "react-router-dom";
 import "../App.css";
 import Dashboard from './Dashboard';
 import Incomes from './Incomes';
@@ -7,20 +7,26 @@ import Expenses from './Expenses';
 
 function UserPage() {
     const { userId } = useParams();
+    const location = useLocation();
     const [activeLink, setActiveLink] = useState(`/UserPage/${userId}/dashboard`);
+
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location]);
+
     return (
         <div style={{ display: "flex", height: "100vh" }}>
             <nav className="sidebar">
                 <h2>Hello {`${userId}`}!</h2>
                 <ul>
-                    <li className={activeLink === `/UserPage/${userId}/dashboard` ? "active" : ""} onClick={() => setActiveLink(`/UserPage/${userId}/dashboard`)}>
-                        <Link to={`/UserPage/${userId}/dashboard`}>Dashboard</Link>
+                    <li className={activeLink === `/UserPage/${userId}/dashboard` ? "active" : ""}>
+                        <Link to={`/UserPage/${userId}/dashboard`} onClick={() => setActiveLink(`/UserPage/${userId}/dashboard`)}>Dashboard</Link>
                     </li>
-                    <li className={activeLink === `/UserPage/${userId}/incomes` ? "active" : ""} onClick={() => setActiveLink(`/UserPage/${userId}/incomes`)}>
-                        <Link to={`/UserPage/${userId}/incomes`}>Income Sources</Link>
+                    <li className={activeLink === `/UserPage/${userId}/incomes` ? "active" : ""}>
+                        <Link to={`/UserPage/${userId}/incomes`} onClick={() => setActiveLink(`/UserPage/${userId}/incomes`)}>Income Sources</Link>
                     </li>
-                    <li className={activeLink === `/UserPage/${userId}/expenses` ? "active" : ""} onClick={() => setActiveLink(`/UserPage/${userId}/expenses`)}>
-                        <Link to={`/UserPage/${userId}/expenses`}>Expenses</Link>
+                    <li className={activeLink === `/UserPage/${userId}/expenses` ? "active" : ""}>
+                        <Link to={`/UserPage/${userId}/expenses`} onClick={() => setActiveLink(`/UserPage/${userId}/expenses`)}>Expenses</Link>
                     </li>
                 </ul>
             </nav>
@@ -29,10 +35,11 @@ function UserPage() {
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="incomes" element={<Incomes />} />
                     <Route path="expenses" element={<Expenses />} />
+                    <Route path="*" element={<Dashboard />} />
                 </Routes>
             </div>
         </div>
-    )
+    );
 }
 
-export default UserPage
+export default UserPage;
