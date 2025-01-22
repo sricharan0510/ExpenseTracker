943d521615c4a7d716d75901be1874cfe65085f8
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-22 18:50:54 +0530

    Commit

--- Exp-Front/src/Components/ExpCat.jsx
+++ Exp-Front/src/Components/ExpCat.jsx
@@ -1,79 +1,79 @@
-// import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
-// import '../App.css'
-// import { Bar } from "react-chartjs-2";
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from "chart.js";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../App.css'
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-// function ExpCat() {
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     axios.get("http://localhost:9000/ExpenseTracker")
-//       .then((res) => {
-//         setData(res.data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   }, []);
+function ExpCat() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:9000/ExpenseTracker")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-//   const expenseCategories = {};
-//   data.forEach(user => {
-//     user.allExpenses.forEach(expObj => {
-//       expObj.Expenses.forEach(exp => {
-//         if (expenseCategories[exp.category]) {
-//           expenseCategories[exp.category] += exp.expenseAmount;
-//         } else {
-//           expenseCategories[exp.category] = exp.expenseAmount;
-//         }
-//       });
-//     });
-//   });
+  const expenseCategories = {};
+  data.forEach(user => {
+    user.allExpenses.forEach(expObj => {
+      expObj.Expenses.forEach(exp => {
+        if (expenseCategories[exp.category]) {
+          expenseCategories[exp.category] += exp.expenseAmount;
+        } else {
+          expenseCategories[exp.category] = exp.expenseAmount;
+        }
+      });
+    });
+  });
 
-//   const barData = {
-//     labels: Object.keys(expenseCategories),
-//     datasets: [
-//       {
-//         label: "Expenses",
-//         data: Object.values(expenseCategories),
-//         backgroundColor: [
-//           '#E5334A',
-//           '#1BA345',
-//           '#FEC000',
-//           '#343A3F',
-//           '#00D6F1',
-//           '#352988',
-//           '#941100'
-//         ],
-//         borderColor: [
-//           '#E5334A',
-//           '#1BA345',
-//           '#FEC000',
-//           '#343A3F',
-//           '#00D6F1',
-//           '#352988',
-//           '#941100'
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   }
+  const barData = {
+    labels: Object.keys(expenseCategories),
+    datasets: [
+      {
+        label: "Expenses",
+        data: Object.values(expenseCategories),
+        backgroundColor: [
+          '#E5334A',
+          '#1BA345',
+          '#FEC000',
+          '#343A3F',
+          '#00D6F1',
+          '#352988',
+          '#941100'
+        ],
+        borderColor: [
+          '#E5334A',
+          '#1BA345',
+          '#FEC000',
+          '#343A3F',
+          '#00D6F1',
+          '#352988',
+          '#941100'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
 
-//   return (
-//     <div>
-//       <h1>Expense Categories</h1> <br></br>
-//       <Bar data={barData}/>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <h1>Expense Categories</h1> <br></br>
+      <Bar data={barData}/>
+    </div>
+  )
+}
 
-// export default ExpCat
+export default ExpCat
--- Exp-FrontEnd/src/Components/Statistics.jsx
+++ Exp-FrontEnd/src/Components/Statistics.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+// Register Chart.js components
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 function Statistics() {
-  return (
-    <div className='container'>
-        <h1>Statistics</h1>
-    </div>
-  )
+    const { userId } = useParams();
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // Fetch data
+    useEffect(() => {
+        axios
+            .get(`http://localhost:9000/${userId}/userExpenses`)
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [userId]);
+
+    const expenseCategories = {};
+    data.forEach(exp => {
+        if (expenseCategories[exp.category]) {
+            expenseCategories[exp.category] += exp.expenseAmount;
+        } else {
+            expenseCategories[exp.category] = exp.expenseAmount;
+        }
+    });
+
+    const chartData = {
+        labels: Object.keys(expenseCategories),
+        datasets: [
+            {
+                label: 'Expenses',
+                data: Object.values(expenseCategories),
+                backgroundColor: [
+                    '#FF6384', // Red
+                    '#36A2EB', // Blue
+                    '#FFCE56', // Yellow
+                    '#4BC0C0', // Teal
+                    '#9966FF', // Purple
+                    '#FF9F40', // Orange
+                    '#5AC18E', // Green
+                ],
+                borderColor: '#ccc',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Category-wise Expenses Breakdown',
+            },
+        },
+    };
+
+    return (
+        <div className="container">
+            <h1>Statistics</h1>
+            <div style={{ width: '80%', margin: '0 auto' }}>
+                <Bar data={chartData} options={options} />
+            </div>
+        </div>
+    );
 }
 
-export default Statistics
+export default Statistics;
